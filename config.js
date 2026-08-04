fd7f302aa30432f03bd1353a2e16759ff6b3459f
lachezar-todorov <lachezar1976@gmail.com>
2026-08-04 14:20:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Постави тук линка от Apps Script (Deploy → Web app), завършващ на /exec
-const API_URL = 'ПОСТАВИ_ТУК_ТВОЯ_APPS_SCRIPT_ЛИНК_ЗАВЪРШВАЩ_НА_/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyRaFveO1v5-86K26BTf64yY-p_jN-OYyyEmsBLwO2X0YIX5kXIbfXzqxhIUevMN9PwVg/exec';
